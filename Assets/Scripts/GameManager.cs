702e79501c935c571cb50cfeefadd54ee30facc2
M. Enes ŞİMŞEK <menes@sabanciuniv.edu>
2021-07-28 13:34:10 +0300

    First upload of the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,124 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using HmsPlugin;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+	#region Singleton class: GameManager
+
+	public static GameManager Instance;
+
+	void Awake ()
+	{
+		if (Instance == null) {
+			Instance = this;
+		}
+	}
+
+	#endregion
+
+	Camera cam;
+
+	public Rock rock;
+	public Trajectory trajectory;
+	[SerializeField] float pushForce = 4f;
+
+	bool isDragging = false;
+
+	Vector2 startPoint;
+	Vector2 endPoint;
+	Vector2 direction;
+	Vector2 force;
+	float distance;
+
+	public Text scoreText;
+	public static int score = 0;
+	public static int rockCount = 5;
+	Rock rockScript;
+	public Text rockCountText;
+
+	public Text finalScoreText;
+
+	public bool isThrowing = false;
+	public bool isRockLeft = true;
+
+	void Start ()
+	{
+		cam = Camera.main;
+		rock.DesactivateRb ();
+		rockScript = GameObject.Find("Rock").GetComponent<Rock>();
+		rockCountText.text = "Rocks Remaining " + rockCount;
+		scoreText.text = "Score: " + score;
+	}
+
+	void Update ()
+	{
+		if(rock != null)
+        {
+			if (!isThrowing)
+			{
+				if (Input.GetMouseButtonDown(0))
+				{
+					isDragging = true;
+					if (isRockLeft)
+						OnDragStart();
+				}
+				if (Input.GetMouseButtonUp(0))
+				{
+					isDragging = false;
+					OnDragEnd();
+				}
+			}
+
+			if (isDragging)
+			{
+				OnDrag();
+			}
+		}
+	}
+
+	void OnDragStart ()
+	{
+		rock.DesactivateRb ();
+		startPoint = cam.ScreenToWorldPoint (Input.mousePosition);
+
+		trajectory.Show ();
+	}
+
+	void OnDrag ()
+	{
+		endPoint = cam.ScreenToWorldPoint (Input.mousePosition);
+		distance = Vector2.Distance (startPoint, endPoint);
+		direction = (startPoint - endPoint).normalized;
+		force = direction * distance * pushForce;
+
+		//just for debug
+		Debug.DrawLine (startPoint, endPoint);
+
+		if (rock != null)
+        {
+			trajectory.UpdateDots(rock.pos, force);
+		}
+		
+	}
+
+	void OnDragEnd ()
+	{
+		rockCount--;
+		if (rockCount == 0)
+			isRockLeft = false;
+		rockCountText.text = "Rocks Remaining " + rockCount;
+
+		//push the rock
+		rock.ActivateRb ();
+
+		rock.Push(force);
+
+		trajectory.Hide ();
+
+		isThrowing = true;
+	}
+
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float minSpeed;
+    public float maxSpeed;
+    float speed;
+    Spawner spawnerScript;
+    int spawnPointSide;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        speed = Random.Range(minSpeed, maxSpeed);
+        spawnerScript = GameObject.Find("Spawner").GetComponent<Spawner>();
+        spawnPointSide = spawnerScript.getSpawnPointSide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawnPointSide = spawnerScript.getSpawnPointSide();
+        
+        if (spawnPointSide == 0) 
+        { 
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitObject)
+    {
+        /*if (hitObject.tag == "Player")
+        {
+            //Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }*/
+
+        if (hitObject.tag == "Ground")
+        {
+            //Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        else if (hitObject.tag == "Rock")
+        {
+            anim.SetBool("shouldYouPop", true);
+            //anim.SetBool("shouldYouPop", false);
+            //Instantiate(explosion, transform.position, Quaternion.identity);
+            //print("HIT BY THE ROCK!");
+            //Destroy(gameObject);
+        }
+    }
+}
